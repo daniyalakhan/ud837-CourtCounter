44cb68ee4d0c48329057d600c63020486f232223
daniyalakhan <daniyalakhan@programmer.net>
2016-12-03 19:02:16 +0500

    Practice Set 2 - Lecture 23, Created a global (variable: scoreTeamA) in MainActivity.java file, 03Dec16

--- CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -7,6 +7,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int scoreTeamA = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
